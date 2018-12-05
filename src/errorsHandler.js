7af445dfb041dc852d47bd70d88948d7000fddab
Yauheni Harokh <Yauheni_Harokh@epam.com>
2018-12-05 15:56:41 +0300

    fix proxy logger and error handling

--- src/errorsHandler.js
+++ src/errorsHandler.js
@@ -1,9 +1,16 @@
 class ErrorsHandler {
   generateErrorMessage(placeholderForNews, error){
-    const errorMessage = document.createElement('h3');
-    errorMessage.innerText = 'Ooops, invalid request...';
-    errorMessage.classList.add('error-message');
-    placeholderForNews.appendChild(errorMessage);
+    const defaultErrorMessage = document.createElement('h3');
+    const trueErrorMessage = document.createElement('h3');
+
+    defaultErrorMessage.innerText = `Ooops, invalid request...`;
+    trueErrorMessage.innerText = error;
+
+    defaultErrorMessage.classList.add('error-message-default');
+    trueErrorMessage.classList.add('error-message-default', 'error-message-true');
+
+    placeholderForNews.appendChild(defaultErrorMessage);
+    placeholderForNews.appendChild(trueErrorMessage);
 
     console.error(error);
   }
--- src/proxy/proxyHandler.js
+++ src/proxy/proxyHandler.js
@@ -3,6 +3,11 @@ import requestFactory from '../requestFactory.js';
 export const handler = {
   get: function(target, prop) {
     target = requestFactory(prop);
+    
+    const { method, body } = target;
+    
+    console.log(`Request was send by method - ${method}`);
+    if (body) console.log(`Request was send with body - ${body}`)
 
     return target;
   },
